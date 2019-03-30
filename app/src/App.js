f5478a909c558f471069fbda13c976702a9614c4
Jonss <joao.jmss@gmail.com>
2019-03-30 15:26:48 -0300

    Desafio top top top - aula 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import BtnBotcamp from './components/BtnBotcamp'
+import BotcampLogo from './components/Logo'
+import "./reset.css"
+import "./app.css"
 
 class App extends Component {
     render() {
-        return <BtnBotcamp />
+        return (
+            <div className="container">
+                <BotcampLogo />
+                <BtnBotcamp />
+            </div>
+        )
     }
 }
 
---
+++ app/src/components/Logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './img/botcamp_2.png';
+import "./style.css"
+
+const BotcampLogo = () => (
+    <img className="botcamp-logo" src={logo} alt="BotcampLogo" />
+)
+
+export default BotcampLogo;
